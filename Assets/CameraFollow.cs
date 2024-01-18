7875d0f4152d864107633a0f28904b17eabdc5f9
Marius Funk <Marius_Funk@gmx.de>
2024-01-19 00:36:06 +0100

    Camera Script update: Level limitation

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -9,6 +9,21 @@ public class CameraFollow : MonoBehaviour
     public Vector3 offset; // Abstand zu dem Objekt
     private Vector3 velocity = Vector3.zero;
 
+    public Transform topLimiter;
+    public float topLimiterOffset;
+    public Transform bottomLimiter;
+    public float bottomLimiterOffset;
+    public Transform rightLimiter;
+    public float rightLimiterOffset;
+    public Transform leftLimiter;
+    public float leftLimiterOffset;
+
+    private float minX;
+    private float maxX;
+    private float minZ;
+    private float maxZ;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +36,9 @@ public class CameraFollow : MonoBehaviour
         if(target != null) // Wenn die target Variable belegt ist, die Kamera also ein Objekt zum verfolgen hat:
         {
             Vector3 targetPosition = target.position + offset; // Fügt der Objekt position den gewählten Offset Hinzu.
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, (leftLimiter.position.x + leftLimiterOffset), (rightLimiter.position.x - rightLimiterOffset));
+            targetPosition.z = Mathf.Clamp(targetPosition.z, (bottomLimiter.position.z + bottomLimiterOffset), (topLimiter.position.z - topLimiterOffset));
 
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime); // Interpoliert von der aktuellen position zur gewünschten, innerhalt der angegebenen Zeit
         }
